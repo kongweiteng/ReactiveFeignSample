049c8d58920ea4ec77e00c2c0c2bd9be32299580
孔维腾 <weiteng.kong@rootcloud.com>
2021-07-07 21:00:18 +0800

    requestmapping 方式

--- server-consumer/src/main/java/com/consumer/sample/ReactiveFeignSample.java
+++ server-consumer/src/main/java/com/consumer/sample/ReactiveFeignSample.java
@@ -1,6 +1,5 @@
 package com.consumer.sample;
 
-import feign.RequestLine;
 import org.springframework.web.bind.annotation.GetMapping;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
@@ -8,7 +7,7 @@ import reactor.core.publisher.Mono;
 @ReactiveFeignClient(name = "server-provider")
 public interface ReactiveFeignSample {
 
-    @RequestLine("GET  /geetings")
+//    @RequestLine("GET  /geetings")
     @GetMapping(value = "/geetings")
     Mono<String> greeting();
 }
